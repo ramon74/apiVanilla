6bbbf49e4f833466b17246453896a8620aa39139
Ramon Abelardo Espinoza Godoy <ramon74@gmail.com>
2025-04-14 14:26:21 -0400

    Incorporación de modelos de entidad relación y actualización a la base de datos para incorporar entidades Lista y Tareas, así como tablas auxiliares y de registro histórico

--- doc/RespaldoDB.sql
+++ doc/RespaldoDB.sql
@@ -31,7 +31,11 @@ CREATE TABLE `desaarrollador` (
   `correo` varchar(100) NOT NULL,
   `idEspecialidad` int DEFAULT '1',
   `idStatus` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_desarrollador_status_idx` (`idStatus`),
+  KEY `fk_desarrollador_especialidad_idx` (`idEspecialidad`),
+  CONSTRAINT `fk_desarrollador_especialidad` FOREIGN KEY (`idEspecialidad`) REFERENCES `especialidad` (`id`),
+  CONSTRAINT `fk_desarrollador_status` FOREIGN KEY (`idStatus`) REFERENCES `status` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -53,8 +57,8 @@ DROP TABLE IF EXISTS `especialidad`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `especialidad` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `descripcion` varchar(500) DEFAULT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  `detalle` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -70,6 +74,73 @@ INSERT INTO `especialidad` VALUES (1,'Desarrollador','Produce pieezas de código
 UNLOCK TABLES;
 
 --
+-- Table structure for table `lista`
+--
+
+DROP TABLE IF EXISTS `lista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lista` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) NOT NULL,
+  `detalle` varchar(500) DEFAULT NULL,
+  `idTipoLista` int NOT NULL,
+  `idDesarrollador` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_lista_tipoLista_idx` (`idTipoLista`),
+  KEY `fk_lista_desarrollador_idx` (`idDesarrollador`),
+  CONSTRAINT `fk_lista_desarrollador` FOREIGN KEY (`idDesarrollador`) REFERENCES `desaarrollador` (`id`),
+  CONSTRAINT `fk_lista_tipoLista` FOREIGN KEY (`idTipoLista`) REFERENCES `tipolista` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lista`
+--
+
+LOCK TABLES `lista` WRITE;
+/*!40000 ALTER TABLE `lista` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lista` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movimientos`
+--
+
+DROP TABLE IF EXISTS `movimientos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movimientos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idDesarrollador` int NOT NULL,
+  `idTarea` int NOT NULL,
+  `inicio` datetime NOT NULL,
+  `fin` datetime DEFAULT NULL,
+  `listaOrigen` int NOT NULL,
+  `listaDestino` int DEFAULT NULL,
+  `mensajeDesarrollador` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_movimientos_desarrollador_idx` (`idDesarrollador`),
+  KEY `fk_movimientos_listaOrigen_idx` (`listaOrigen`),
+  KEY `fk_movimientos_listaDestino_idx` (`listaDestino`),
+  KEY `fk_movimientos_tarea_idx` (`idTarea`),
+  CONSTRAINT `fk_movimientos_desarrollador` FOREIGN KEY (`idDesarrollador`) REFERENCES `desaarrollador` (`id`),
+  CONSTRAINT `fk_movimientos_listaDestino` FOREIGN KEY (`listaDestino`) REFERENCES `lista` (`id`),
+  CONSTRAINT `fk_movimientos_listaOrigen` FOREIGN KEY (`listaOrigen`) REFERENCES `lista` (`id`),
+  CONSTRAINT `fk_movimientos_tarea` FOREIGN KEY (`idTarea`) REFERENCES `tarea` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Esta tabla contendra todos los movimientos generados entre tareas y listas\nSI listaOrigen y listaDestino son iguales, será la creación de la tarea\nSI listaDestino es la lista Especial "En Desarrollo" será una toma de tarea por el desarrollador\nSI listaOrigen es la lista Especial "En Desarrollo" será la liberación de tarea por el desarrollador que la colocó en "En Desarrollo"\nLa listaOrigen de un movimiento debe ser la listaDestino del movimiento inmediato anterior\nSI la listaDestino de un movimiento apunta a una lista de tipo Terminal, ya no puede registrar nuevos movimientos';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movimientos`
+--
+
+LOCK TABLES `movimientos` WRITE;
+/*!40000 ALTER TABLE `movimientos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movimientos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `status`
 --
 
@@ -78,7 +149,7 @@ DROP TABLE IF EXISTS `status`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `status` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(15) DEFAULT NULL,
+  `descripcion` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -92,6 +163,55 @@ LOCK TABLES `status` WRITE;
 INSERT INTO `status` VALUES (1,'Habilitado'),(2,'Deshabilitado');
 /*!40000 ALTER TABLE `status` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tarea`
+--
+
+DROP TABLE IF EXISTS `tarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tarea` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) NOT NULL,
+  `detalle` varchar(500) DEFAULT NULL,
+  `prioridad` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tarea`
+--
+
+LOCK TABLES `tarea` WRITE;
+/*!40000 ALTER TABLE `tarea` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tarea` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipolista`
+--
+
+DROP TABLE IF EXISTS `tipolista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipolista` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipolista`
+--
+
+LOCK TABLES `tipolista` WRITE;
+/*!40000 ALTER TABLE `tipolista` DISABLE KEYS */;
+INSERT INTO `tipolista` VALUES (1,'Normal'),(2,'Terminal'),(3,'Especial');
+/*!40000 ALTER TABLE `tipolista` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -102,4 +222,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-10 22:58:00
+-- Dump completed on 2025-04-14 14:20:34
